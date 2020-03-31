26b20c3e90e1f02628ce2235de8520e6d45dd1c7
Mauricio <mauriciojr.dev@gmail.com>
2020-03-31 10:50:06 -0300

    Criando primeiros arquivos do projeto

---
+++ main.js
@@ -0,0 +1,70 @@
+let inputElemnt = document.querySelector('input');
+let addButton = document.querySelector('.add');
+let clearButton = document.querySelector('.clear');
+let listElement = document.querySelector('ul');
+let sortButton = document.querySelector('.sort');
+let deleteButton = document.querySelector('.delete');
+let noteElement = document.querySelector('.notification');
+
+var amigos = [
+]
+
+function reload() {
+    amigos.map((item, index) => {
+        let listText = document.createTextNode(item);
+        let itemElement = document.createElement("li");
+        itemElement.appendChild(listText);
+        listElement.appendChild(itemElement);
+
+    });
+}
+
+//função para adicionar novo amigo
+addButton.onclick = () => {
+    listElement.innerHTML = '';
+    amigos.push(inputElemnt.value);
+    reload();
+    console.log(inputElemnt.value + ' adicionado com sucesso!')
+    inputElemnt.value = '';
+
+}
+
+//função para limpar lista de amigos
+clearButton.onclick = () => {
+    listElement.innerHTML = '';
+    amigos.length = 0
+
+}
+
+//função para sortear
+function sorteio() {
+    if (amigos.length == 0) {
+
+        noteElement.setAttribute('class', 'notification is-warning');
+
+    } else {
+        sortButton.setAttribute('class', 'button is-primary is-rounded sort is-loading');
+        var x = Math.floor((Math.random() * amigos.length));
+        console.log('Sorteando...');
+        let map = amigos.map((item, index) => {
+            if (index == x) {
+                setTimeout(() => {
+                    alert(item + ' ira pagar a proxima cerveja!');
+                    sortButton.setAttribute('class', 'button is-primary is-rounded sort');
+                }, 1000);
+
+            }
+
+        });
+
+    }
+
+}
+
+function deleteNote() {
+
+    noteElement.setAttribute('class','is-hidden')
+
+}
+
+reload();
